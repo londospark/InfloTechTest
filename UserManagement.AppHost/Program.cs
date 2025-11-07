ec4aaf5e1023068ef73a053b5127777d830612cf
Gareth Hubball <ridecar2@gmail.com>
2025-11-07 14:29:03 +0000

    feat: Neaten up Aspire Dashboard

--- UserManagement.AppHost/Program.cs
+++ UserManagement.AppHost/Program.cs
@@ -2,6 +2,8 @@ using Aspire.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.UserManagement_Web>("usermanagement-web");
+builder.AddProject<Projects.UserManagement_Web>("usermanagement-web")
+    .WithUrlForEndpoint("https", url => url.DisplayText = "Home")
+    .WithUrlForEndpoint("http", url => url.DisplayText = "Home (http)");
 
 builder.Build().Run();
